e031f88f22c1ed44fb88ee0ae92acd1873687fa7
Yuree <yj.yuree.jang@gmail.com>
2020-06-01 13:07:33 -0700

    Resolved compile warnings

--- src/components/Logo.js
+++ src/components/Logo.js
@@ -3,7 +3,7 @@ import React from "react";
 class Logo extends React.Component {
     render() {
         return (
-            <img src={"https://i.pinimg.com/236x/ce/07/83/ce07832fec3120543529c213f71fd722.jpg"}/>
+            <img src={"https://i.pinimg.com/236x/ce/07/83/ce07832fec3120543529c213f71fd722.jpg"} alt="Message Board"/>
         );
     }
 }
